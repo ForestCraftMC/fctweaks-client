47a06ae0e0b77bafd15f9adbbd09d260aa732c50
任天天 <ruattqwq@gmail.com>
2025-03-03 21:20:12 +0800

    Improve: add log

--- src/main/java/io/github/ruattd/fc/tweaks/ForestCraftTweaks.java
+++ src/main/java/io/github/ruattd/fc/tweaks/ForestCraftTweaks.java
@@ -5,10 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
-import reactor.netty.http.Http11SslContextSpec;
+import org.apache.logging.log4j.LogManager;
 import reactor.netty.http.server.HttpServer;
-
-import java.io.File;
 
 public final class ForestCraftTweaks implements ModInitializer {
     @Getter
@@ -16,19 +14,25 @@ public final class ForestCraftTweaks implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        log("Initializing...");
         CommandRegistrationCallback.EVENT.register(CommandAfk::register);
         ServerLifecycleEvents.SERVER_STARTED.register(server -> ForestCraftTweaks.server = server);
-//        var cert = new File("/etc/letsencrypt/live/fc.nijika.in/fullchain.pem");
-//        var key = new File("/etc/letsencrypt/live/fc.nijika.in/privkey.pem");
-        var httpServer = HttpServer.create();
-        Runnable r = () -> httpServer.port(80)
-                .route(routes -> {
-                    routes.get("/style.css", HttpCss::route);
-                    routes.get("/ping", HttpPing::route);
-                    routes.get("/", Http::route);
-                })
-//                .secure(spec -> spec.sslContext(Http11SslContextSpec.forServer(cert, key)))
-                .bindNow();
-        new Thread(r, "Status server").start();
+        var serverThread = new Thread(() -> {
+            var httpServer = HttpServer.create();
+            httpServer.port(25580)
+                    .route(routes -> {
+                        routes.get("/style.css", HttpCss::route);
+                        routes.get("/ping", HttpPing::route);
+                        routes.get("/", Http::route);
+                    })
+                    .bindNow();
+            log("Builtin status server started on *:25580");
+        }, "Status server");
+        serverThread.start();
+        log("Tweaks applied");
+    }
+
+    public static void log(String message) {
+        LogManager.getLogger("ForestCraftTweaks").info(message);
     }
 }

d79bbbec380e3c68dd928469c22e8e00f78f9c32
任天天 <ruattqwq@gmail.com>
2025-03-03 22:06:57 +0800

    Fix: single player client loading

--- src/main/java/io/github/ruattd/fc/tweaks/ForestCraftTweaks.java
+++ src/main/java/io/github/ruattd/fc/tweaks/ForestCraftTweaks.java
@@ -6,7 +6,6 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
 import org.apache.logging.log4j.LogManager;
-import reactor.netty.http.server.HttpServer;
 
 public final class ForestCraftTweaks implements ModInitializer {
     @Getter
@@ -17,18 +16,6 @@ public final class ForestCraftTweaks implements ModInitializer {
         log("Initializing...");
         CommandRegistrationCallback.EVENT.register(CommandAfk::register);
         ServerLifecycleEvents.SERVER_STARTED.register(server -> ForestCraftTweaks.server = server);
-        var serverThread = new Thread(() -> {
-            var httpServer = HttpServer.create();
-            httpServer.port(25580)
-                    .route(routes -> {
-                        routes.get("/style.css", HttpCss::route);
-                        routes.get("/ping", HttpPing::route);
-                        routes.get("/", Http::route);
-                    })
-                    .bindNow();
-            log("Builtin status server started on *:25580");
-        }, "Status server");
-        serverThread.start();
         log("Tweaks applied");
     }
 
